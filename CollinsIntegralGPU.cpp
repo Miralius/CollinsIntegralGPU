35e1a48e05c35e9100bc6adee3d0ccf823debf0a
Frolov <f-miralius@yandex.ru>
2020-02-03 20:44:05 +0400

    Начато написание программы, вычисляющей двумерный интеграл Коллинза.

--- CollinsIntegralGPU.cpp
+++ CollinsIntegralGPU.cpp
@@ -1,20 +1,81 @@
-// CollinsIntegralGPU.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+inline void error(const std::string& s)
+{
+	throw runtime_error(s);
+}
+
+double funcGauss2D(double a, int n1, double* xy, double** func, double sigma) {
+	double h = 2 * a / n1;
+
+	return h;
+}
+
+void collins2D(double a, double b, double A, double B, double C, double D, int n1, int n2, double n, double wavelength) {
+	double* xy = new double[n1];
+	double** func = new double* [n1 * n1];
+	for (int i = 0; i < n1; i++) {
+		func[i] = new double[n1];
+	}
+	double h;
+	cout << "Входная функция:" << "\nГауссов пучок (1): ";
+	string select;
+	cin >> select;
+	if (select == "1") {
+		cout << "Введите параметр сигма:" << "\nsigma = ";
+		double sigma;
+		cin >> sigma;
+		h = funcGauss2D(a, n1, xy, func, sigma);
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+	try {
+		cout << "Расчёт одномерного интеграла Коллинза…" << endl;
+		while (1) {
+			cout << "Введите пределы интегрирования (a и b):" << "\na = ";
+			double a, b;
+			cin >> a;
+			if (a == 0) break;
+			cout << "b = ";
+			cin >> b;
+
+			double A, B, C, D;
+			cout << "Введите коэффициенты ABCD-матрицы: " << endl;
+			cin >> A >> B >> C >> D;
+			if ((A * D - B * C) != 1) error("Определитель ABCD-матрицы должен быть равен 1");
+
+			cout << "Введите количество отсчётов интегрирования (n входного поля и n выходного поля):" << "\nn1 = ";
+			int n1, n2;
+			cin >> n1;
+			cout << "n2 = ";
+			cin >> n2;
+
+			cout << "Введите число завихрения световой волны:" << "\nn = ";
+			double n;
+			cin >> n;
+
+			cout << "Введите длину волны света:" << "\nwavelength = ";
+			double wavelength;
+			cin >> wavelength;
+
+			collins2D(a, b, A, B, C, D, n1, n2, n, wavelength);
+
+			cout << "Продолжить расчёты? Для выхода ввести 0" << endl;
+		}
+	}
+	catch (runtime_error & e) {
+		cerr << "Ошибка! " << e.what() << endl;
+	}
+	catch (...) {
+		cerr << "Неизвестная ошибка!" << endl;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
