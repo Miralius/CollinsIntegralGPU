6f5836a22ee8a0754d3f38a86dc521a5be4ccc00
Frolov <f-miralius@yandex.ru>
2020-02-07 13:11:10 +0400

    Уменьшено количество переменных, убраются повторяющие куски кода

--- CollinsIntegralGPU.cpp
+++ CollinsIntegralGPU.cpp
@@ -12,33 +12,49 @@ inline void error(const string& s)
 	throw runtime_error(s);
 }
 
-void vortex(vector<vector<double>> func, double n, int n1, vector<double> xy, vector<vector<complex<double>>>& funcVortex) {
-	for (int i = 0; i < n1; i++) {
-		funcVortex.push_back(vector<complex<double>>());
-		for (int j = 0; j < n1; j++) {
-			funcVortex.at(i).push_back(func.at(i).at(j) * exp(complex<double>(0, (n * ((j < (n1 / 2)) ? atan2(-xy.at(i), xy.at(j)) : (atan2(-xy.at(i), xy.at(j)) + 2 * PI))))));
-		}
+vector<double> calcPoints(double interval, double count) {
+	double pointValue = -interval;
+	vector<double> points;
+	double h = 2 * interval / count;
+
+	for (int i = 0; i < count; i++) {
+		points.push_back(pointValue);
+		pointValue += h;
 	}
+
+	return points;
 }
 
-double funcGauss2D(double a, int n1, vector<double>& xy, vector<vector<double>>& func, double sigma) {
-	double h = 2 * a / n1;
-	double xyValue = -a;
-	vector<double> func1D;
-
-	for (int i = 0; i < n1; i++) {
-		xy.push_back(xyValue);
-		func1D.push_back((exp(-(xyValue * xyValue) / (2 * sigma * sigma))));
-		xyValue += h;
+vector<vector<double>> functionGauss(vector<double> xy) {
+	cout << "Введите параметр сигма:" << "\nsigma = ";
+	double sigma;
+	cin >> sigma;
+	
+	vector<double> function1D;
+	for (int i = 0; i < xy.size(); i++) {
+		function1D.push_back((exp(-(xy.at(i) * xy.at(i)) / (2 * sigma * sigma))));
 	}
 
-	for (int i = 0; i < n1; i++) {
-		func.push_back(vector<double>());
-		for (int j = 0; j < n1; j++) {
-			func.at(i).push_back(func1D.at(i) * func1D.at(j));
+	vector<vector<double>> input;
+	for (int i = 0; i < function1D.size(); i++) {
+		input.push_back(vector<double>());
+		for (int j = 0; j < function1D.size(); j++) {
+			input.at(i).push_back(function1D.at(i) * function1D.at(j));
 		}
 	}
-	return h;
+
+	return input;
+}
+
+vector<vector<complex<double>>> vortex(vector<vector<double>> func, vector<double> xy, double n) {
+	vector<vector<complex<double>>> functionVortex;
+	for (int i = 0; i < xy.size(); i++) {
+		functionVortex.push_back(vector<complex<double>>());
+		for (int j = 0; j < xy.size(); j++) {
+			functionVortex.at(i).push_back(func.at(i).at(j) * exp(complex<double>(0, (n * ((j < (xy.size() / 2)) ? atan2(-xy.at(i), xy.at(j)) : (atan2(-xy.at(i), xy.at(j)) + 2 * PI))))));
+		}
+	}
+	return functionVortex;
 }
 
 void integrating2D_B0(double b, double n2, vector<vector<complex<double>>> funcVortex, double wavelength, double C, double D) {
@@ -49,28 +65,13 @@ void integrating2D(double b, double n2, double h, vector<vector<complex<double>>
 
 }
 
-void collins2D(double a, double b, double A, double B, double C, double D, int n1, int n2, double n, double wavelength) {
-	vector<double> xy;
-	vector<vector<double>> input;
-
-	double h{};
-	cout << "Входная функция:" << "\nГауссов пучок (1): ";
-	string select;
-	cin >> select;
-	if (select == "1") {
-		cout << "Введите параметр сигма:" << "\nsigma = ";
-		double sigma;
-		cin >> sigma;
-		h = funcGauss2D(a, n1, xy, input, sigma);
-	}
-
-	vector<vector<complex<double>>> funcVortex;
-	vortex(input, n, n1, xy, funcVortex);
+void collins2D(double a, double b, vector<vector<double>> matrixABCD, int n1, int n2, double n, double wavelength) {
+	
 
 	vector<vector<complex<double>>> outputField;
-	if (abs(B) < DBL_EPSILON) {
-		integrating2D_B0(b, n2, funcVortex, wavelength, C, D);
-	} else integrating2D(b, n2, h, funcVortex, xy, wavelength, A, B, D, outputField);
+	//if (abs(matrixABCD.at(0).at(1)) < DBL_EPSILON) {
+	//	integrating2D_B0(b, n2, funcVortex, wavelength, C, D);
+	//} else integrating2D(b, n2, h, funcVortex, xy, wavelength, A, B, D, outputField);
 
 	cout << "Введите имя файла результатов: ";
 	string nameFile;
@@ -92,10 +93,22 @@ int main()
 			cout << "b = ";
 			cin >> b;
 
-			double A, B, C, D;
 			cout << "Введите коэффициенты ABCD-матрицы: " << endl;
-			cin >> A >> B >> C >> D;
-			if ((A * D - B * C) != 1) error("Определитель ABCD-матрицы должен быть равен 1");
+
+			double coefficient;
+			vector<vector<double>> matrixABCD;
+			
+			for (int i = 0; i < 2; i++) {
+				matrixABCD.push_back(vector<double>());
+				for (int j = 0; j < 2; j++) {
+					cin >> coefficient;
+					matrixABCD.at(i).push_back(coefficient);
+				}
+			}
+			
+			if ((matrixABCD.at(0).at(0) * matrixABCD.at(1).at(1) - matrixABCD.at(0).at(1) * matrixABCD.at(1).at(0)) != 1) {
+				error("Определитель ABCD-матрицы должен быть равен 1");
+			}
 
 			cout << "Введите количество отсчётов интегрирования (n входного поля и n выходного поля):" << "\nn1 = ";
 			int n1, n2;
@@ -111,7 +124,24 @@ int main()
 			double wavelength;
 			cin >> wavelength;
 
-			collins2D(a, b, A, B, C, D, n1, n2, n, wavelength);
+			double h = 2 * a / n1;
+
+			vector<double> xy = (abs(matrixABCD.at(0).at(1)) < DBL_EPSILON) ? calcPoints(b, n2) : calcPoints(a, n1); //
+			vector<double> uv = calcPoints(b, n2);
+
+			vector<vector<double>> input;
+			cout << "Входная функция:" << "\nГауссов пучок (1): ";
+			string select;
+			cin >> select;
+			if (select == "1") {
+				input = functionGauss(xy);
+			}
+			else {
+				error("Не выбрана входная функция!");
+			}
+
+			vector<vector<complex<double>>> funcVortex = vortex(input, xy, n);
+
 
 			cout << "Продолжить расчёты? Для выхода ввести 0" << endl;
 		}
