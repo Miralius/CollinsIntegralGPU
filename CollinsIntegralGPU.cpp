677494a45b09cf08269dc131d91d737509088106
Frolov <f-miralius@yandex.ru>
2020-05-19 23:49:21 +0400

    Исправлены все ошибки, проект готов к внедрению CUDA

--- CollinsIntegralGPU.cpp
+++ CollinsIntegralGPU.cpp
@@ -165,15 +165,15 @@ vector<vector<complex<double>>> collins(vector<vector<complex<double>>> function
 	double k = 2 * PI / wavelength;
 	int startTime(clock() / CLOCKS_PER_SEC), endTime(clock() / CLOCKS_PER_SEC), currentTime(clock() / CLOCKS_PER_SEC), progress(0);
 	vector<vector<complex<double>>> output;
-	for (int p = 0; p < u.size(); p++) {
-		processing(++progress, (int)u.size(), clock() / CLOCKS_PER_SEC - currentTime, (endTime - startTime) * ((int)u.size() - p));
+	for (int p = 0; p < v.size(); p++) {
+		processing(++progress, (int)v.size(), clock() / CLOCKS_PER_SEC - currentTime, (endTime - startTime) * ((int)v.size() - p));
 		startTime = endTime;
 		output.push_back(vector<complex<double>>());
-		for (int q = 0; q < v.size(); q++) {
+		for (int q = 0; q < u.size(); q++) {
 			complex<double> value = 0;
-			for (int i = 0; i < x.size(); i++) {
-				for (int j = 0; j < y.size(); j++) {
-					value += functionVortex.at(i).at(j) * exp(complex<double>(0, ((k / (2 * matrixABCD.at(0).at(1))) * (matrixABCD.at(0).at(0) * (x.at(i) * x.at(i) + y.at(j) * y.at(j)) - 2 * (x.at(i) * u.at(p) + y.at(j) * v.at(q)) + matrixABCD.at(1).at(1) * (u.at(p) * u.at(p) + v.at(q) * v.at(q))))));
+			for (int i = 0; i < y.size(); i++) {
+				for (int j = 0; j < x.size(); j++) {
+					value += functionVortex.at(i).at(j) * exp(complex<double>(0, ((k / (2 * matrixABCD.at(0).at(1))) * (matrixABCD.at(0).at(0) * (y.at(i) * y.at(i) + x.at(j) * x.at(j)) - 2 * (y.at(i) * v.at(p) + x.at(j) * u.at(q)) + matrixABCD.at(1).at(1) * (v.at(p) * v.at(p) + u.at(q) * u.at(q))))));
 				}
 			}
 			output.at(p).push_back(complex<double>(0, -(k / (2 * PI * matrixABCD.at(0).at(1)))) * value * hx * hy);
